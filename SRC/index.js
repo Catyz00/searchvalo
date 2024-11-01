c9bdb7a40d9eae01bcc6e99d61da31afd032ba5c
xMargi <viniciusff200@gmail.com>
2024-11-01 16:17:38 -0300

    V1.0.7

--- SRC/index.js
+++ SRC/index.js
@@ -1,4 +1,34 @@
-function showNavBar(){
-    const getNavBarFromHtml = document.getElementById("navbar")
-    
+function showNavBar() {
+    const getHamburguerFromHtml = document.getElementById("Hamburguer");
+    const getNavBarFromHtml = document.getElementById("navbar");
+
+    getHamburguerFromHtml.addEventListener("click", () => {
+        if (getNavBarFromHtml.classList.contains("navbarOn")) {
+            getNavBarFromHtml.classList.remove("navbarOn");
+            getNavBarFromHtml.classList.add("navbarOff");
+        } else {
+            getNavBarFromHtml.classList.remove("navbarOff");
+            getNavBarFromHtml.classList.add("navbarOn");
+        }
+    });
 }
+
+function transformHamburguerToLineX() {
+    const getHamburguerFromHtml = document.getElementById("Hamburguer");
+    const getLinesFromHamburguer = document.querySelectorAll(".linha");
+
+    getHamburguerFromHtml.addEventListener("click", () => {
+        getLinesFromHamburguer.forEach(line => {
+            if (line.classList.contains("linha")) {
+                line.classList.remove("linha");
+                line.classList.add("linhaX");
+            } else {
+                line.classList.remove("linhaX");
+                line.classList.add("linha");
+            }
+        });
+    });
+}
+
+showNavBar();
+transformHamburguerToLineX();
